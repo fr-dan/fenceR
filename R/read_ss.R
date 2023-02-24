66b8d15a24aed4ff608dc74ba06bceeda46843dd
fr-dan <daniel.braby@forestresearch.gov.uk>
2023-02-24 15:32:34 +0000

    v1

--- R/read_ss.R
+++ R/read_ss.R
@@ -15,7 +15,20 @@ read_ss = function(folder = folder, file = file) {
            mill_name = 2,
            dom_soft = 3,
            dom_hard = 4,
-           )
+           imp_soft = 5,
+           imp_hard = 6,
+           eng_soft = 7,
+           eng_hard = 8,
+           sco_soft = 9,
+           sco_hard = 10,
+           wal_soft = 11,
+           wal_hard = 12,
+           ni_soft = 13,
+           ni_hard = 14,
+           rf_soft = 15,
+           rf_hard = 16,
+           bio_soft = 17,
+           bio_hard = 18)
 }
 
 
